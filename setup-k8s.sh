06e0d4caa493cc50d1890e54b71a997435722d0c
Tiago Posse <tiagoposse@gmail.com>
2021-03-23 15:51:10 +0100

    continuation

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -1,24 +1,37 @@
 
 apply="kubectl apply -f"
 
+deploy() {
+  for fname in $(tr '\n' ' ' < $1/.deploy); do
+    $apply $1/$fname;
+  done
+}
+
 $apply cluster/*
 
-helm upgrade -i helm-operator fluxcd/helm-operator -n flux --create-namespace -f helm-operator/values.yml
-$apply ingress/release.yml
+# deploy certmanager
+# deploy dashboard
+# deploy ingress
+# deploy registry
+# deploy smallstep
+deploy vault
 
-$apply $(tr '\n' ' ' < certmanager/.deploy)
-$apply $(tr '\n' ' ' < registry/release.yml)
-$apply $(tr '\n' ' ' < museum/release.yml)
-$apply $(tr '\n' ' ' < smallstep/.deploy)
-$apply $(tr '\n' ' ' < vault/.deploy)
 
-while [ "$(kubectl get po -n vault | grep -E 'vault-\d' | grep 'Running' | wc -l | xargs)" != "3" ]
-do
-  echo "$(kubectl get po -n vault | grep -E 'vault-\d' | grep 'Running' | wc -l | xargs)"
-  sleep 2
-done
+# helm upgrade -i helm-operator fluxcd/helm-operator -n flux --create-namespace -f helm-operator/values.yml
+# $apply ingress/release.yml
 
-echo $(kubectl exec -ti -n vault vault-0 -- cat /tmp/vault-init.json) > vault-init.json
+# $apply $(tr '\n' ' ' < certmanager/.deploy)
+# $apply $(tr '\n' ' ' < registry/release.yml)
+# $apply $(tr '\n' ' ' < museum/release.yml)
+# $apply $(tr '\n' ' ' < vault/.deploy)
 
-$apply $(tr '\n' ' ' < kscp/release.yml)
-$apply $(tr '\n' ' ' < cicd/.deploy)
+# while [ "$(kubectl get po -n vault | grep -E 'vault-\d' | grep 'Running' | wc -l | xargs)" != "3" ]
+# do
+#   echo "$(kubectl get po -n vault | grep -E 'vault-\d' | grep 'Running' | wc -l | xargs)"
+#   sleep 2
+# done
+
+# echo $(kubectl exec -ti -n vault vault-0 -- cat /tmp/vault-init.json) > vault-init.json
+
+# $apply $(tr '\n' ' ' < kscp/release.yml)
+# $apply $(tr '\n' ' ' < cicd/.deploy)
